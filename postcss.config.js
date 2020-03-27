0a95e700abda64c728a8068de614e69edf66628a
hank <764671440@qq.com>
2020-03-27 11:40:31 +0800

    init

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,6 @@ module.exports = {
   plugins: {
     autoprefixer: {
       // browsers: ['Android >= 4.0', 'iOS >= 7']
-    },
-    "postcss-pxtorem": {
-      rootValue: 37.5, //136
-      propList: ["*"]
     }
   }
 };
